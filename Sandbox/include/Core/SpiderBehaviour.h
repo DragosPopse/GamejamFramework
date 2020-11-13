73bec22b2b44154676f0daa64f05cc1c1ef24181
Bonniebon <jan.vijfhuizen@outlook.com>
2020-11-13 14:45:30 +0100

    Finished basic version of the spider.
    I should implement A* but there's no time left.

---
+++ Sandbox/include/Core/SpiderBehaviour.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Gamejam/JECS/Core/ISystemBehaviour.h"
+#include <Core\SpiderBrain.h>
+#include "Gamejam/JECS/Core/SystemManager.h"
+
+namespace jam
+{
+	class SpiderBehaviour final : jecs::ISystemBehaviour<SpiderBrain>
+	{
+	public:
+		SpiderBehaviour(jecs::SystemManager& manager);
+		void Update();
+	};
+}
